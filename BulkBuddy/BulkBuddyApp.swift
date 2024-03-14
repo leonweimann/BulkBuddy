d93ea31b84146e82cde08ef0bd0848a37cf2d763
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-03-14 19:49:57 +0100

    Initial Commit

---
+++ BulkBuddy/BulkBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulkBuddyApp.swift
+//  BulkBuddy
+//
+//  Created by Leon Weimann on 14.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BulkBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
