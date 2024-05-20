e2129e049ffbc32d6400520446a204303ed9a220
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-05-20 14:13:17 +0200

    [Feature] Add currentUser to AppViewModel + basic according methods

--- BulkBuddy/Models/View/AppViewModel.swift
+++ BulkBuddy/Models/View/AppViewModel.swift
@@ -19,15 +19,48 @@ final class AppViewModel {
         self.router = router
     }
     
+    // MARK: Config
     let applicationState: ApplicationState
     
+    @ObservationIgnored
+    @Injected(\.authClient) private var authClient
+    
+    @ObservationIgnored
+    @Injected(\.datastoreClient) private var datastoreClient
+    
+    // MARK: Routing
     var router: AnyRouter
     
+    /// Route to **WelcomeView**.
     static var WelcomeRoute = AnyRoute(.fullScreenCover) { _ in
         NavigationView {
             WelcomeView()
         }
     }
-        
+    
+    // MARK: Properties / Data
+    var currentUser: User?
+    
+    // MARK: Methods
     static func createFrom(appState: ApplicationState, with router: AnyRouter) -> AppViewModel { AppViewModel(state: appState, router: router) }
+    
+    func pullUserData() async {
+        guard let authUser = authClient.authUser else { return } // force user to sign in
+        
+        do {
+            currentUser = try await datastoreClient.get(User.self, for: authUser.uid) // while get function is awaited, show loading screen (beautiful version ;D)
+        } catch {
+            // Handle error via 'router'
+        }
+    }
+    
+    func pushUserData() async {
+        guard let currentUser else { return } // handle issue via alert -> 'router'
+        
+        do {
+            try await datastoreClient.set(data: currentUser) // optionally present beautiful loading screen (if func was initiated by user itself)
+        } catch {
+            // Handle error via 'router'
+        }
+    }
 }
