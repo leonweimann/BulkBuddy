222d21fd6301cdd727897e0aef9300a2df0fa92f
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-05-21 21:38:02 +0200

    [Clean] Refactored AppProgressScreen + Added animating color to text indicator

--- BulkBuddy/Core/AppProgressScreen.swift
+++ BulkBuddy/Core/AppProgressScreen.swift
@@ -10,10 +10,13 @@ import SwiftUI
 // MARK: - AppProgressScreen
 
 struct AppProgressScreen: View {
-    @State private var animate = false
+    // MARK: Config
+    var animationSpeed = 0.2
     var contentImages = ["qrcode.viewfinder", /*"barcode.viewfinder",*/ "creditcard.viewfinder"]
-    
     static private let loaderTitle = Array("BulkBuddy")
+    
+    // MARK: Animation
+    @State private var animate = false
     @State private var loadingIndication = Array(repeating: false, count: loaderTitle.count)
     @State private var animationPeriod = 0
     
@@ -45,34 +48,6 @@ struct AppProgressScreen: View {
         }
         .onAppear { animate = true }
         .onDisappear { animate = false }
-    }
-    
-    private var showAdditionalLoadingIndicator: Bool { animationPeriod > 2 }
-    
-    private var additionalLoadingIndicatorContent: String {
-        guard let currentBouncedChar = loadingIndication.firstIndex(of: true) else { return "." }
-        let dotCount = Int(currentBouncedChar / 3)
-        return String(repeating: ".", count: dotCount + 1)
-    }
-    
-    private func animateCharRecursively(current index: Int = 0) {
-        guard animate else { return }
-        
-        guard index < Self.loaderTitle.count else {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                animateCharRecursively()
-            }
-            
-            animationPeriod += 1
-            return
-        }
-        
-        loadingIndication[index] = true
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            animateCharRecursively(current: index + 1)
-            loadingIndication[index] = false
-        }
     }
     
     private var appIconBase: some View {
@@ -115,12 +90,27 @@ struct AppProgressScreen: View {
     private var appNameText: some View {
         HStack(alignment: .bottom, spacing: 0) {
             ForEach(0 ..< Self.loaderTitle.count, id: \.self) { index in
+                let isCurrent = loadingIndication[index]
+                
                 Text(String(Self.loaderTitle[index]))
-                    .offset(y: loadingIndication[index] ? -4 : 0)
+                    .foregroundStyle(isCurrent ? .secondary : .primary)
+                    .offset(y: isCurrent ? -4 : 0)
             }
         }
         .font(.largeTitle)
         .fontWeight(.bold)
+    }
+    
+    private var additionalLoadingIndicator: some View {
+        additionalLoadingIndicatorText("...")
+            .opacity(0)
+            .overlay(alignment: .leading) {
+                additionalLoadingIndicatorText(additionalLoadingIndicatorContent)
+                    .foregroundStyle(.secondary)
+                    .contentTransition(.numericText())
+            }
+            .offset(y: showAdditionalLoadingIndicator ? 32 : 0)
+            .opacity(showAdditionalLoadingIndicator ? 1 : 0)
     }
     
     private func additionalLoadingIndicatorText(_ string: String) -> some View {
@@ -129,18 +119,36 @@ struct AppProgressScreen: View {
             .fontWeight(.bold)
     }
     
-    private var additionalLoadingIndicator: some View {
-        additionalLoadingIndicatorText("...")
-            .opacity(0)
-            .overlay(alignment: .leading) { additionalLoadingIndicatorAnimated }
-            .offset(y: showAdditionalLoadingIndicator ? 32 : 0)
-            .opacity(showAdditionalLoadingIndicator ? 1 : 0)
+    // MARK: Dynamic Config
+    private var currentBouncedChar: Int? { loadingIndication.firstIndex(of: true) }
+    
+    private var showAdditionalLoadingIndicator: Bool { animationPeriod > 2 }
+    
+    // MARK: Animation Logic
+    private var additionalLoadingIndicatorContent: String {
+        guard let currentBouncedChar else { return "." }
+        let dotCount = Int(currentBouncedChar / 3)
+        return String(repeating: ".", count: dotCount + 1)
     }
     
-    private var additionalLoadingIndicatorAnimated: some View {
-        additionalLoadingIndicatorText(additionalLoadingIndicatorContent)
-            .foregroundStyle(.secondary)
-            .contentTransition(.numericText())
+    private func animateCharRecursively(current index: Int = 0) {
+        guard animate else { return }
+        
+        guard index < Self.loaderTitle.count else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + animationSpeed) {
+                animateCharRecursively()
+            }
+            
+            animationPeriod += 1
+            return
+        }
+        
+        loadingIndication[index] = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + animationSpeed) {
+            animateCharRecursively(current: index + 1)
+            loadingIndication[index] = false
+        }
     }
 }
 
