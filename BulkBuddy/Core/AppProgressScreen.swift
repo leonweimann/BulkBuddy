d9fc82ccd256dc3236fe092beecbb7d55ec244e5
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-05-21 21:30:34 +0200

    [Feature] Add loading indication + additional indicator to AppProgressScreen

--- BulkBuddy/Core/AppProgressScreen.swift
+++ BulkBuddy/Core/AppProgressScreen.swift
@@ -11,12 +11,17 @@ import SwiftUI
 
 struct AppProgressScreen: View {
     @State private var animate = false
-    var contentImages = ["qrcode.viewfinder", "creditcard.viewfinder"]
+    var contentImages = ["qrcode.viewfinder", /*"barcode.viewfinder",*/ "creditcard.viewfinder"]
+    
+    static private let loaderTitle = Array("BulkBuddy")
+    @State private var loadingIndication = Array(repeating: false, count: loaderTitle.count)
+    @State private var animationPeriod = 0
     
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(.background)
+                .onTapGesture { animate.toggle() }
             
             VStack(alignment: .center, spacing: 32) {
                 ZStack {
@@ -27,8 +32,46 @@ struct AppProgressScreen: View {
                 }
                 .frame(width: 200, height: 200)
                 
-                appNameText
+                ZStack {
+                    additionalLoadingIndicator
+                        .animation(.bouncy, value: showAdditionalLoadingIndicator)
+                        .animation(.default, value: additionalLoadingIndicatorContent)
+                    
+                    appNameText
+                        .onChange(of: animate) { if $1 { animateCharRecursively() } }
+                        .animation(.bouncy, value: loadingIndication)
+                }
             }
+        }
+        .onAppear { animate = true }
+        .onDisappear { animate = false }
+    }
+    
+    private var showAdditionalLoadingIndicator: Bool { animationPeriod > 2 }
+    
+    private var additionalLoadingIndicatorContent: String {
+        guard let currentBouncedChar = loadingIndication.firstIndex(of: true) else { return "." }
+        let dotCount = Int(currentBouncedChar / 3)
+        return String(repeating: ".", count: dotCount + 1)
+    }
+    
+    private func animateCharRecursively(current index: Int = 0) {
+        guard animate else { return }
+        
+        guard index < Self.loaderTitle.count else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                animateCharRecursively()
+            }
+            
+            animationPeriod += 1
+            return
+        }
+        
+        loadingIndication[index] = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            animateCharRecursively(current: index + 1)
+            loadingIndication[index] = false
         }
     }
     
@@ -71,12 +114,33 @@ struct AppProgressScreen: View {
     
     private var appNameText: some View {
         HStack(alignment: .bottom, spacing: 0) {
-            ForEach(Array("BulkBuddy"), id: \.self) { char in
-                Text(String(char))
+            ForEach(0 ..< Self.loaderTitle.count, id: \.self) { index in
+                Text(String(Self.loaderTitle[index]))
+                    .offset(y: loadingIndication[index] ? -4 : 0)
             }
         }
         .font(.largeTitle)
         .fontWeight(.bold)
+    }
+    
+    private func additionalLoadingIndicatorText(_ string: String) -> some View {
+        Text(string)
+            .font(.title)
+            .fontWeight(.bold)
+    }
+    
+    private var additionalLoadingIndicator: some View {
+        additionalLoadingIndicatorText("...")
+            .opacity(0)
+            .overlay(alignment: .leading) { additionalLoadingIndicatorAnimated }
+            .offset(y: showAdditionalLoadingIndicator ? 32 : 0)
+            .opacity(showAdditionalLoadingIndicator ? 1 : 0)
+    }
+    
+    private var additionalLoadingIndicatorAnimated: some View {
+        additionalLoadingIndicatorText(additionalLoadingIndicatorContent)
+            .foregroundStyle(.secondary)
+            .contentTransition(.numericText())
     }
 }
 
@@ -87,3 +151,4 @@ struct AppProgressScreen: View {
         AppProgressScreen()
     }
 }
+ 
