5715c11ba1b94a4b19551f89a66df9fe8f49e5a5
Leon Weimann <130781996+leonweimann@users.noreply.github.com>
2024-06-30 19:54:57 +0200

    [Feat] Add wrappedError helper to UserManager

--- BulkBuddy/Managers/UserManager.swift
+++ BulkBuddy/Managers/UserManager.swift
@@ -10,7 +10,15 @@ import Foundation
 // MARK: - UserManager
 
 final actor UserManager {
+    @Injected(\.authClient) private var auth
+    @Injected(\.datastoreClient) private var datastore
 
+    private func wrappedError(_ error: any Error) -> UserError {
+        switch error {
+        case is FirestoreError: return UserError.internalIssue(error as! FirestoreError)
+        default: return error as! UserError // This may cause app to crash. Would be the signal to check, which case isn't handled
+        }
+    }
 }
 
 // MARK: - Injection Key
